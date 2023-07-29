e028c7f8f2bfe7812fe500164c91d57c46eda0b2
rajprince320 <rajprince320@gmail.com>
2023-07-30 00:09:45 +0530

    update

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ async function Links() {
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
   );
   const links = await link.json();
-  let content = document.querySelector(".grid-box");
-  for (let i = 0; i < links.length; i++) {
-    content.innerHTML += `
-    <div class="content">
-      <div class="title">
-        <img width="50px" src="${links[i].image}"/>
-        <div>
-          <h3>${links[i].name}</h3>
-          <p>${links[i].id}</p>
-        </div>
-      </div>
-      <div class="percent">${links[i].price_change_percentage_24h + "%"}</div>
-      <div class="price">${"$" + links[i].current_price}</div>
-      <p class="text">Total Volume: ${links[i].total_volume}</p>
-      <p class="text">Market Cap: ${links[i].market_cap}</p>
-    </div>`;
+  // let content = document.querySelector(".grid-box");
+  // for (let i = 0; i < links.length; i++) {
+  //   content.innerHTML += `
+  //   <div class="content">
+  //     <div class="title">
+  //       <img width="50px" src="${links[i].image}"/>
+  //       <div>
+  //         <h3>${links[i].name}</h3>
+  //         <p>${links[i].id}</p>
+  //       </div>
+  //     </div>
+  //     <div class="percent">${links[i].price_change_percentage_24h + "%"}</div>
+  //     <div class="price">${"$" + links[i].current_price}</div>
+  //     <p class="text">Total Volume: ${links[i].total_volume}</p>
+  //     <p class="text">Market Cap: ${links[i].market_cap}</p>
+  //   </div>`;
 
-    let contentList = document.querySelector(".list-box");
-    for (let i = 0; i < links.length; i++) {
-      contentList.innerHTML += `
-    <div class="content">
+  let contentList = document.querySelector(".list-box");
+  for (let i = 0; i < links.length; i++) {
+    contentList.innerHTML += `
+    <div class="contentt">
       <div class="title">
         <img width="50px" src="${links[i].image}"/>
         <div>
@@ -36,8 +36,11 @@ async function Links() {
       <p class="texts">Total Volume: ${links[i].total_volume}</p>
       <p class="texts">Market Cap: ${links[i].market_cap}</p>
     </div>`;
-    }
   }
+
+  // if (links[i].price_change_percentage_24h < 0) {
+  //   // document.getElementsByClassName("percent").style.color = "red";
+  // }
 }
 
 const tabs = document.querySelectorAll(".tab-btn");
